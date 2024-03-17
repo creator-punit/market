0bba821b1fe7c76cf5aa547ab90a45ee0acddf0f
Punit <3033.punit@gmail.com>
2024-03-17 19:06:54 +0530

    feat: Delete user API completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,7 +36,6 @@ const registerUser = async (req, res) => {
     };
     const createdUser = await User.create(user);
 
-    console.log(createdUser, "----------------------");
     if (!createdUser) {
       res.status(500).send({
         status: 0,
@@ -74,27 +73,24 @@ const loginUser = async (req, res) => {
       };
     }
 
-    const exist = await User.findAll(where);
+    const exist = await User.findAll({where});
 
-    if (!exist) {
+    if (!exist.length) {
       res.send({
         status: 0,
         message: "No account exists with the given credentials",
       });
     }
-const user = exist[0].dataValues;
-    await bcrypt.compare(
-      password,
-      exist[0].dataValues.password,
-      async function (err, result) {
-        if (!result) {
-          res.send({
-            status: 0,
-            message: "The entered password is incorrect",
-          });
-        }
+    const user = exist[0].dataValues;
+
+    await bcrypt.compare(password, user.password, async function (err, result) {
+      if (!result) {
+        res.send({
+          status: 0,
+          message: "The entered password is incorrect",
+        });
       }
-    );
+    });
 
     const userDetails = {
       name: user.firstname + " " + user.lastname,
@@ -107,7 +103,7 @@ const user = exist[0].dataValues;
     res.send({
       status: 1,
       userDetails,
-      message: "User successfully loggedIn",
+      message: "User is successfully loggedIn",
     });
   } catch (error) {
     throw error;
@@ -117,7 +113,67 @@ const updateUser = async (req, res) => {
   const { phone, email, password } = req.body;
 };
 const deleteUser = async (req, res) => {
-  const { phone, email, password } = req.body;
+  try {
+    const { phone, email, password } = req.body;
+
+    if (!phone && !email) {
+      return res.send({
+        status: 0,
+        message: "user phone number or email required",
+      });
+    }
+
+    let where;
+
+    if (phone) {
+      where = {
+        phone,
+      };
+    } else {
+      where = {
+        email,
+      };
+    }
+
+    const exist = await User.findAll({where});
+
+    if (!exist.length) {
+      return res.send({
+        status: 0,
+        message: "No account exists with the given credentials",
+      });
+    }
+
+    const user = exist[0].dataValues;
+    let isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.send({
+        status: 0,
+        message: "The entered password is incorrect",
+      });
+    }
+
+    const userDeleted = await User.destroy({
+      where: {
+        phone,
+      },
+    });
+
+    if (!userDeleted) {
+      return res.send({
+        status: 0,
+        message: "Internal error deleting User",
+      });
+    }
+
+    return res.send({
+      status: 1,
+      message: "User is successfully deleted",
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 export { registerUser, loginUser, updateUser, deleteUser };
