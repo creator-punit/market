2a97a7280e67f54a2c15e6d6c3bd53629ad0da84
Punit <3033.punit@gmail.com>
2024-06-02 12:25:18 +0530

    feat: created error handler

---
+++ src/utils/handler/error.handler.js
@@ -0,0 +1,7 @@
+const handleError = ({ status, title, message }) => {
+  return {
+    status: status || "500",
+    title: title || "Internal Server Error!",
+    message: message || "Error in processing request! Please try again later.",
+  };
+};
